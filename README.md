6ed76b8667d20ea7d70427929837b468ceefa55c
Ali <bulatova@sheridancollege.ca>
2020-05-03 07:14:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mern-shopping-list
+ A simple MERN stack shopping list application
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://admin:Qwe321@cluster0-v7ot7.mongodb.net/test?retryWrites=true&w=majority'
+}
