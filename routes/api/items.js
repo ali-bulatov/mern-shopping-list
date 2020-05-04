575c799ec68c5ebf579be89137b2874d5d2cb6cb
Ali <bulatova@sheridancollege.ca>
2020-05-03 21:09:14 -0400

    Frontend Completed

--- routes/api/items.js
+++ routes/api/items.js
@@ -38,6 +38,6 @@ router.delete('/:id', (req,res) => {
             then(() => res.json({success: true})
             .catch(err => console.log(err))))
         .catch(err => res.status(404).json({success: false}));
-})
+});
 
 module.exports = router;
